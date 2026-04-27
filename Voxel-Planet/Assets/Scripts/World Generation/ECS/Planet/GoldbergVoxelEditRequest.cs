607ace952eb1369b3121323bda04946ad1cdae3a
ByGravityyy <jamesgardiner99@gmail.com>
2026-04-28 00:07:50 +0100

    removed some files

--- Voxel-Planet/Assets/Scripts/World Generation/ECS/Planet/GoldbergVoxelEditRequest.cs
+++ Voxel-Planet/Assets/Scripts/World Generation/ECS/Planet/GoldbergVoxelEditRequest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VoxelPlanetComponents : MonoBehaviour
+public class GoldbergVoxelEditRequest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
